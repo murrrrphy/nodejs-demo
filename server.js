48e51cb64ca6dc924575d5d8cf91c52acdbadcd7
murphy <987127314@qq.com>
2020-10-16 16:16:18 +0800

    修改请求时提示的话

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var server = http.createServer(function (request, response) {
 
     /******** 从这里开始看，上面不要看 ************/
 
-    console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
+    console.log('收到请求！请求路径（带查询参数）为：' + pathWithQuery)
 
     if (path === '/') {
         response.statusCode = 200
